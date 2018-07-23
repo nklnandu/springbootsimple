3f7a766093dca22c30cf94778cb91f9be1f56ebd
NanduVani <NanduVani@NAVA>
2018-07-22 21:46:04 -0500

    update

--- src/main/java/com/simplecrud/dao/CustomerDAO.java
+++ src/main/java/com/simplecrud/dao/CustomerDAO.java
@@ -1,5 +1,6 @@
 package com.simplecrud.dao;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,11 +14,11 @@ public class CustomerDAO {
 	// Dummy database. Initialize with some dummy values.
 	private static List<Customer> customers;
 	{
-		customers = new ArrayList();
+		customers = new ArrayList<>();
 		customers.add(new Customer(101, "John", "Doe", "djohn@gmail.com", "121-232-3435"));
 		customers.add(new Customer(201, "Russ", "Smith", "sruss@gmail.com", "343-545-2345"));
 		customers.add(new Customer(301, "Kate", "Williams", "kwilliams@gmail.com", "876-237-2987"));
-		customers.add(new Customer(System.currentTimeMillis(), "Viral", "Patel", "vpatel@gmail.com", "356-758-8736"));
+		customers.add(new Customer(LocalTime.now().toNanoOfDay(), "Viral", "Patel", "vpatel@gmail.com", "356-758-8736"));
 	}
 
 	/**
@@ -25,7 +26,7 @@ public class CustomerDAO {
 	 * 
 	 * @return list of customers
 	 */
-	public List list() {
+	public List<Customer> list() {
 		return customers;
 	}
 
